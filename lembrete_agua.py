1b696ccb86250f85d76ea7f683728cd25a92accd
Gustavo Duarte <donhaduarte@gmail.com>
2025-07-16 21:58:56 -0300

    1° versão projeto beber água

---
+++ lembrete_agua.py
@@ -0,0 +1,3 @@
+print("Curiosidade: a palavra 'água' é um(a)", type("água"))
+qtd_agua = float(input("informe quantos litros de água você bebeu até agora: "))
+print("Parabéns, você bebeu ", qtd_agua , " litros de água")
