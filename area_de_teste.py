0ab897165e671634784ed41b586d6a9f4f3c923d
unknown <o.dvgusta916.dd@gmail.com>
2025-08-18 15:32:07 -0300

    Atualização 2.36 - Correção 3 do crud - projeto beber água

--- area_de_teste.py
+++ area_de_teste.py
@@ -87,7 +87,7 @@ while True:
                 while True:
                     nome_a_ser_alterado = input("Digite o nome desejado: ")
                     if len(nome_a_ser_alterado) >= 3:
-                        lista_a_recuperar_salva.insert(indice_externo, lista_geral_users[indice_externo])
+                        lista_a_recuperar_salva = lista_geral_users[indice_externo]
                         del lista_geral_users[indice_externo][indice_interno]
                         lista_geral_users[indice_externo].insert(indice_interno, nome_a_ser_alterado)
                         print("Valor alterado com sucesso!")
@@ -95,6 +95,7 @@ while True:
                         break
                     else:
                         print("Erro: Nome deve possuir pelo menos 3 caracteres!")
+                print(lista_geral_users)
                 while True:
                     quantidade_a_ser_alterada = input("Digite a quantidade desejada: ")
                     if quantidade_a_ser_alterada.isdigit() == True:
@@ -124,6 +125,12 @@ while True:
                     break
                 else:
                     print("E")
+            except ValueError:
+                print("Por favor, digite um tipo correto!")
+            except IndexError:
+                print("Índice não existe na lista")
+            except Exception:
+                print("Erro Descon")
         while indice_interno not in [0, 1]:
             deseja_alterar = input("Deseja alterar o [N]ome, [Q]uantidade ou [T]odos: ").lower().strip()
             if deseja_alterar == "n":
